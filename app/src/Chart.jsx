93da1ff631b41f25119eb04eafa43792c09637b8
dausyla <dausyla@gmail.com>
2024-09-11 11:11:31 +0200

    hierarchy and chart separated sections

---
+++ app/src/Chart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Chart() {
+  return (
+    <div class="container">
+      <h1>Chart</h1>
+    </div>
+  );
+}
+
+export default Chart;
---
+++ app/src/Hierarchy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hierarchy() {
+  return (
+    <div class="container">
+      <h1>Hierarchy</h1>
+    </div>
+  );
+}
+
+export default Hierarchy;
