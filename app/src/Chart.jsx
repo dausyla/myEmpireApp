2b06b459d01f4787f7504a4144338bfbfb6b9475
dausyla <dausyla@gmail.com>
2024-09-11 12:12:31 +0200

    First Chart!

--- app/src/Chart.jsx
+++ app/src/Chart.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import {React, useState} from 'react';
+import { Line } from 'react-chartjs-2'
+import {Chart as ChartJS} from 'chart.js/auto'
+import { fakeData } from './Data';
+
+function formatData(_data) {
+    const cfg = {
+        datasets: [{
+            data: [{ x: '2016-12-25', y: 20 }, { x: '2016-12-26', y: 10 }]
+        }]
+    }
+    const [finalData, setFinalData] = useState(cfg);
+
+    console.log(finalData);
+    return finalData
+}
 
 function Chart() {
-  return (
-    <div class="container">
-      <h1>Chart</h1>
-    </div>
-  );
+    const data = formatData(fakeData);
+
+    return (
+        <div class="container">
+            <Line data={data} />
+        </div>
+    );
 }
 
 export default Chart;
---
+++ app/src/Data.jsx
@@ -0,0 +1,8 @@
+export const fakeData = {
+    stocks: {
+        values: [500, 600, 650]
+    },
+    immo:{
+        values: [750, 500, 600]
+    }
+};
