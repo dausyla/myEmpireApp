fa4b27e7891d4749a7372da56944338f4c6b5a33
Gabriel Daure <gabriel.daure@epita.fr>
2025-09-22 23:42:06 +0200

    feat: detailed graph

--- MyEmpireApp/src/components/pages/myEmpireAppComponents/predictionsComponents/graphs/Graph.tsx
+++ MyEmpireApp/src/components/pages/myEmpireAppComponents/predictionsComponents/graphs/Graph.tsx
@@ -2,78 +2,56 @@ import { Line } from "react-chartjs-2";
 import { usePortfolio } from "../../../../../contexts/DataContext/PortfolioContextHook";
 import { Form, InputGroup } from "react-bootstrap";
 import { useState } from "react";
-import { getDates, getPredictedValues } from "./predictionUtils";
-import type { Color } from "../../../../../types/Assets";
-
-const getFadedColor = (color: Color, alpha: number = 0.5) => {
-  return `rgba(${color.r}, ${color.g}, ${color.b}, ${alpha})`; // Return a string in rgba format with alpha
-};
-
-const getColorString = (c: Color) => {
-  return `rgba(${c.r}, ${c.g}, ${c.b})`; // Return a string in rgba format
-};
+import { getDataset, graphOptions } from "./predictionUtils";
 
 export function Graphs() {
   const { portfolio } = usePortfolio();
 
-  const [overMonths, setOverMonths] = useState(12);
+  const [overYears, setOverYears] = useState(2);
+  const [detailPrediction, setDetailPrediction] = useState(false);
 
-  const onMonthsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onYearsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     const parsedValue = Number(value);
+    e.target.value = parsedValue.toString(); // Ensure the input field shows a valid number
     if (!isNaN(parsedValue) && parsedValue >= 0) {
-      setOverMonths(parsedValue);
+      if (parsedValue > 99) {
+        setOverYears(99);
+      } else {
+        setOverYears(parsedValue);
+      }
     } else {
-      setOverMonths(0);
+      setOverYears(0);
     }
   };
 
-  const options = {
-    responsive: true,
-    maintainAspectRatio: true,
-    scales: {
-      y: {
-        stacked: true,
-        min: 0,
-      },
-    },
-    plugins: {
-      filler: {
-        propagate: false,
-      },
-      legend: {
-        position: "top" as const,
-      },
-      title: {
-        display: true,
-        text: "Portfolio Value Over Time",
-      },
-    },
-  };
-
-  const datasets = portfolio.assets.map((asset) => ({
-    label: asset.name,
-    data: getPredictedValues(asset, overMonths),
-    borderColor: getColorString(asset.color),
-    backgroundColor: getFadedColor(asset.color),
-    fill: true,
-  }));
-
-  const data = {
-    labels: getDates(portfolio.dates, overMonths).map(
-      (date) => new Date(date).toISOString().split("T")[0]
-    ),
-    datasets,
-  };
+  const dataset = getDataset(portfolio, overYears, detailPrediction);
 
   return (
     <>
-      <InputGroup>
-        <InputGroup.Text>Prediction over:</InputGroup.Text>
-        <Form.Control onChange={onMonthsChange} value={overMonths} />
-        <InputGroup.Text>months</InputGroup.Text>
-      </InputGroup>
-      <Line options={options} data={data} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <InputGroup style={{ width: "17rem", marginBottom: "1rem" }}>
+          <InputGroup.Text>Prediction over</InputGroup.Text>
+          <Form.Control
+            type="number"
+            onChange={onYearsChange}
+            value={overYears}
+          />
+          <InputGroup.Text>years</InputGroup.Text>
+        </InputGroup>
+        <Form.Check
+          type="switch"
+          label="Detail Prediction"
+          value={detailPrediction ? "on" : "off"}
+          onChange={() => setDetailPrediction(!detailPrediction)}
+        />
+      </div>
+      <Line options={graphOptions} data={dataset} />
     </>
   );
 }
--- MyEmpireApp/src/components/pages/myEmpireAppComponents/predictionsComponents/graphs/predictionUtils.ts
+++ MyEmpireApp/src/components/pages/myEmpireAppComponents/predictionsComponents/graphs/predictionUtils.ts
@@ -1,14 +1,33 @@
+import type { Color, Portfolio } from "../../../../../types/Assets";
 import type { Asset } from "../../../../../types/Assets";
 
-// Returns the dates lists + overMonths dates after the last date in the list
-export function getDates(dates: number[], overMonths: number) {
+export function getDataset(
+  portfolio: Portfolio,
+  overYears: number,
+  detail: boolean = false
+) {
+  const datasets = portfolio.assets.flatMap((asset) =>
+    getDatasetForAsset(asset, overYears, detail)
+  );
+
+  const data = {
+    labels: getDates(portfolio.dates, overYears).map(
+      (date) => new Date(date).toISOString().split("T")[0]
+    ),
+    datasets,
+  };
+
+  return data;
+}
+
+function getDates(dates: number[], overYears: number) {
   if (dates.length === 0) return [];
 
   const lastDate = new Date(dates[dates.length - 1]);
   const newDates = [];
 
   const nextDate = new Date(lastDate);
-  for (let i = 0; i < overMonths; i++) {
+  for (let i = 0; i < overYears * 12; i++) {
     nextDate.setMonth(nextDate.getMonth() + 1);
     if (nextDate.getMonth() === 0) {
       nextDate.setFullYear(nextDate.getFullYear() + 1);
@@ -19,17 +38,92 @@ export function getDates(dates: number[], overMonths: number) {
   return [...dates, ...newDates];
 }
 
-export function getPredictedValues(asset: Asset, overMonths: number) {
-  const predictedValues = [];
-  let lastValue = asset.values[asset.values.length - 1];
-  const monthlyInput = asset.prediction.monthlyInput;
-  const estimatedAPY = asset.prediction.estimatedAPY;
+function getDatasetForAsset(
+  asset: Asset,
+  overYears: number,
+  detail: boolean = false
+) {
+  const color = getColorString(asset.color);
+  const fadedColor = getFadedColor(asset.color, 0.5);
 
-  for (let i = 0; i < overMonths; i++) {
-    const newValue = lastValue * (1 + estimatedAPY / 12) + monthlyInput;
-    predictedValues.push(newValue);
-    lastValue = newValue;
+  const inputs = asset.inputs.reduce<number[]>((acc, input, i) => {
+    if (i === 0) {
+      acc.push(input);
+    } else {
+      acc.push(acc[i - 1] + input);
+    }
+    return acc;
+  }, []);
+  const totalValues = asset.values.slice();
+  const interests = asset.values.map((v, i) => v - inputs[i]);
+
+  const monthlyInput = asset.prediction.monthlyInput || 0;
+  const estimatedAPY = asset.prediction.estimatedAPY || 0;
+  for (let i = 0; i < overYears * 12; i++) {
+    const lastValue = totalValues[totalValues.length - 1];
+    const monthlyGrowth = lastValue * (estimatedAPY / 12);
+
+    const newValue = lastValue + monthlyGrowth + monthlyInput;
+    totalValues.push(newValue);
+    inputs.push(inputs[inputs.length - 1] + monthlyInput);
+    interests.push(newValue - inputs[inputs.length - 1]);
   }
 
-  return [...asset.values, ...predictedValues];
+  return detail
+    ? [
+        {
+          label: `${asset.name} - Inputs`,
+          data: inputs,
+          borderColor: color,
+          backgroundColor: fadedColor,
+          fill: true,
+        },
+        {
+          label: `${asset.name} - Interests`,
+          data: interests,
+          borderColor: color,
+          backgroundColor: fadedColor,
+          fill: true,
+        },
+      ]
+    : [
+        {
+          label: asset.name,
+          data: totalValues,
+          borderColor: color,
+          backgroundColor: fadedColor,
+          fill: true,
+        },
+      ];
 }
+
+export const graphOptions = {
+  responsive: true,
+  maintainAspectRatio: true,
+  scales: {
+    y: {
+      stacked: true,
+      min: 0,
+    },
+  },
+  plugins: {
+    filler: {
+      propagate: false,
+    },
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Portfolio Value Over Time",
+    },
+  },
+};
+
+const getFadedColor = (color: Color, alpha: number = 0.5) => {
+  return `rgba(${color.r}, ${color.g}, ${color.b}, ${alpha})`; // Return a string in rgba format with alpha
+};
+
+const getColorString = (c: Color) => {
+  return `rgba(${c.r}, ${c.g}, ${c.b})`; // Return a string in rgba format
+};
