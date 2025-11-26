ec94ab62759ef76ac42809fab97c41b8510904b7
dausyla <dausyla@gmail.com>
2025-11-26 12:22:14 +0100

    feat: Navbar used to open windows

--- client/src/pages/app/MyEmpireApp.tsx
+++ client/src/pages/app/MyEmpireApp.tsx
@@ -1,24 +1,17 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
-import { Assets } from "./components/assets/Assets";
-import { useWallet } from "../../contexts/WalletContext/WalletContextHook";
-// import { SelectWallet } from "./components/SelectWallet";
-import { useState, useRef } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useRef } from "react";
 import { NavBar } from "./components/Navbar";
 import { AssetHierarchy } from "./components/AssetHierarchy/AssetHierarchy";
 import { useWindowManager } from "../../utilies/components/WindowManager";
-// import { Dashboard } from "./components/dashboard/Dashboard";
-// import { Predictions } from "./components/predictions/Predictions";
 
 export function MyEmpireApp() {
-  // const { wallet } = useWallet();
-  const [currentNav, setCurrentNav] = useState("dashboard");
   const containerRef = useRef<HTMLDivElement>(null);
 
   const { windows, openWindow } = useWindowManager({ containerRef });
 
   return (
     <>
-      <NavBar setCurrentNav={setCurrentNav} />
+      <NavBar openWindow={openWindow} />
 
       <Container
         ref={containerRef}
@@ -31,19 +24,6 @@ export function MyEmpireApp() {
             <AssetHierarchy />
           </Col>
         </Row>
-
-        {/* New Window Button - Absolutely positioned */}
-        <Button
-          onClick={() => openWindow(<Assets />, "Assets")}
-          style={{
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-            zIndex: 1001,
-          }}
-        >
-          New Window
-        </Button>
 
         {/* Render Windows */}
         {windows}
--- client/src/pages/app/components/Navbar.tsx
+++ client/src/pages/app/components/Navbar.tsx
@@ -1,46 +1,61 @@
 import { NavDropdown } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useWallet } from "../../../contexts/WalletContext/WalletContextHook";
+import type { JSX } from "react";
+import { AssetPerformence } from "./assets/AssetPerformence";
+import { AssetValuesTable } from "./assets/AssetValuesTable";
+import { EditAsset } from "./assets/EditAsset";
 
 export function NavBar({
-  setCurrentNav,
+  openWindow,
 }: {
-  setCurrentNav: (nav: string) => void;
+  openWindow: (element: JSX.Element, title: string) => void;
 }) {
-  const { wallet, walletList } = useWallet();
+  const { wallet, walletList, getWallet } = useWallet();
 
   if (!walletList || !wallet) return null;
+
+  const handleWalletClick = (walletId: number) => {
+    if (walletId === wallet.wallet.id) return;
+    getWallet(walletId);
+  };
 
   return (
     <Navbar sticky="top" expand="lg" className="bg-body-tertiary shadow-sm">
       <Container>
-        <Navbar.Brand onClick={() => setCurrentNav("predictions")}>
-          MyEmpireApp
-        </Navbar.Brand>
+        <Navbar.Brand>MyEmpireApp</Navbar.Brand>
         <Navbar.Toggle aria-controls="main-nav" />
         <Navbar.Collapse id="main-nav">
           <NavDropdown title="Wallets" id="basic-nav-dropdown">
             {walletList.map((w, i) => (
               <NavDropdown.Item
                 key={i}
-                // TODO
-                // onClick={() => setCurrentPortfolioId(portfolios[index].id)}
+                onClick={() => handleWalletClick(walletList[i].id)}
               >
                 {w.title}
               </NavDropdown.Item>
             ))}
           </NavDropdown>
-          <Nav className="me-auto align-items-center">
-            <Nav.Link onClick={() => setCurrentNav("dashboard")}>
-              Dashboard
-            </Nav.Link>
-            <Nav.Link onClick={() => setCurrentNav("predictions")}>
-              Predictions
-            </Nav.Link>
-            <Nav.Link onClick={() => setCurrentNav("assets")}>Assets</Nav.Link>
-          </Nav>
+          <NavDropdown title="Windows" id="basic-nav-dropdown" className="ms-3">
+            <NavDropdown.Item
+              onClick={() =>
+                openWindow(<AssetPerformence />, "Asset Performance")
+              }
+            >
+              Asset Performance
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              onClick={() => openWindow(<AssetValuesTable />, "Asset Values")}
+            >
+              Asset Values
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              onClick={() => openWindow(<EditAsset />, "Edit Asset")}
+            >
+              Edit Asset
+            </NavDropdown.Item>
+          </NavDropdown>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end" style={{ gap: 8 }}>
           {wallet.wallet.title}
